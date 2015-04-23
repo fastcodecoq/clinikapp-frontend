9e2964e60b94e02cfb4e8a7366df4dacff3512ac
Gomosoft <jgomez@soluntech.com>
2015-04-22 22:24:42 -0500

    latest

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,19 @@
 // angular config
+
+var app = angular.module('clinikapp', ['ngMaterial', 'ui.router']);
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+
+  $stateProvider
+    .state('elements', {
+      url: "/elements",
+      templateUrl: "views/elements.html"
+    })    
+    ;
+
+  $urlRouterProvider.otherwise("/elements");
+
+
+});
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,1 +1,104 @@
 // controllers 
+
+var app = angular.module('clinikapp');
+
+
+function mainCtrl($scope, $mdDialog, $mdSidenav){
+
+
+		 if(window.history.length > 0)
+		 	 $scope.back = true;
+  		 
+  		 $scope.todos = [];
+
+  		 for(i = 0; i < 6; i++)
+  		 	$scope.todos.push({
+			    face: 'assets/img/logo.png',
+			    what: 'Brunch this weekend?',
+			    who: 'Min Li Chan',
+			    notes: "I'll be in your neighborhood doing errands."
+  			});
+
+
+	      $scope.menu_right = function(){
+	        	$mdSidenav("right").toggle();
+	      }
+
+		  $scope.showAlert = function(ev) {
+		   
+		    $mdDialog.show(
+		      $mdDialog.alert()
+		        .title('Alerta con titulo')
+		        .content('Especificar descripción aquí.')
+		        .ariaLabel('Alerta demo')
+		        .ok('Aceptar')
+		        .targetEvent(ev)
+		    );
+
+       };
+
+
+     $scope.showAlertLeft = function(ev) {
+		   
+		    $mdDialog.show(
+		      $mdDialog.alert()
+		        .title('Swipe')
+		        .content('Haz hecho swipe left.')
+		        .ariaLabel('Swipe')
+		        .ok('Aceptar')
+		        .targetEvent(ev)
+		    );
+
+    };
+
+
+}
+
+
+function entityCtrlBase($scope, $rootScope, $stateParams){
+
+   
+   //basic Methods
+
+
+   $scope.load = function(id){
+
+   	  var id = id || $scope.id;
+
+   }
+
+
+   $scope.create = function(data){
+
+   	  var data = data || $scope;
+
+   }
+
+
+   $scope.update = function(id){
+
+   	  var id = id || $scope.id;
+
+
+   }
+
+
+   $scope.delete = function(id){
+
+   	  var id = id || $scope.id;
+
+
+   }
+
+
+   //other methods 
+
+
+}
+
+
+
+app
+.controller('mainCtrl', mainCtrl)
+.controller('entityCtrlBase', entityCtrlBase)
+;

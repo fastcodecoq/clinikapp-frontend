5565d6df67cdaac2e016a8c6cbfbb1f27524c9cd
Gomosöft <info@gomosoft.com>
2015-04-25 21:50:22 -0500

    latest

--- assets/js/api_service.js
+++ assets/js/api_service.js
@@ -20,11 +20,12 @@ function API(){
 		 	var centers = [
 		 	{
 		    _id : '97162182912a8s7as8a9',		    
-		 	name:'clinica del caribe',		 	
-		 	description : 'Clinica del caribe, brindamos la mejor atencion en el sector.',
+		 	name:'clinica de montería',		 	
+		 	description : 'Clinica de Montería, brindamos la mejor atencion en el sector.',
 		 	tel : 3017681,
 		 	uci:true,
 		 	urgencias:true,
+		 	image : 'assets/img/clinicas/monteria.jpg',
 		 	doctors : [
 		 	        {_id : '98127621612891029asa7612', name:'Juan Alberto', especiality : 'Ortopeedia'},
 		 	        {_id : '98127621612891029asa7612', name:'Janna Zakzuk', especiality : 'Cardiología'},
@@ -35,11 +36,12 @@ function API(){
 		 	},
 		 	{
 		    _id : '97162182912aohh123',		 		
-		 	name:'clinica santa maria',		 		 
-		 	description : 'En la clinica santa maria, contamos con los mejores equipos en radiología a nivel de la costa.',
+		 	name:'clinica de la mujer',		 		 
+		 	description : 'En la Clinica de la Mujer, contamos con los mejores equipos en radiología a nivel de la costa.',
 		 	tel : 3017682,
 		 	uci:false,
 		 	urgencias:true,
+		 	image : 'assets/img/clinicas/clinica_mujer.jpg',		 			 	
 		 	doctors : [
 		 	        {_id : '98127621612891029asa7612', name:'Juan Alberto', especiality : 'Ortopeedia'},
 		 	        {_id : '98127621612891029asa7612', name:'Janna Zakzuk', especiality : 'Cardiología'},
@@ -50,9 +52,10 @@ function API(){
 		 	},
 		 	{
 		    _id : 'kasja812912a8s7as8a9',		 		
-		 	name:'clinica del norte',		 	 
-		 	description : 'En la clinica del norte, contamos con los mejores especialistas en pediatría de la costa norte.',
+		 	name:'clinica soma',		 	 
+		 	description : 'En la soma, contamos con los mejores especialistas en pediatría de la costa norte.',
 		 	tel : 3017683,
+		 	image : 'assets/img/clinicas/soma.gif',		 			 	
 		 	doctors : [
 		 	        {_id : '98127621612891029asa7612', name:'Juan Alberto', especiality : 'Ortopeedia'},
 		 	        {_id : '98127621612891029asa7612', name:'Janna Zakzuk', especiality : 'Cardiología'},
@@ -63,11 +66,12 @@ function API(){
 		 	},
 		 	{
 		    _id : '0812klasias712',		 		
-		 	name:'clinica porto azul',		 		 
-		 	description : 'En la clinica porto azul, contamos con la mejor sala de urgencias de la costa',
+		 	name:'clinica de occidente',		 		 
+		 	description : 'En la clinica de Occidente, contamos con la mejor sala de urgencias',
 		 	uci:false,
 		 	urgencias:false,
 		 	tel : 3017685,
+		 	image : 'assets/img/clinicas/occidente.jpg',		 	
 		 	doctors : [
 		 	        {_id : '98127621612891029asa7612', name:'Juan Alberto', especiality : 'Ortopeedia'},
 		 	        {_id : '98127621612891029asa7612', name:'Janna Zakzuk', especiality : 'Cardiología'},
@@ -78,11 +82,12 @@ function API(){
 		 	},
 		 	{
 		 	_id : "lkmklmsad8981271",
-		 	name:'clinica madre bernarda',		 			 
-		 	description : 'En la clinica madre bernarda, ofrecemos el mejor servicio de UCI en colombia',
+		 	name:'clinica mcomfenalco',		 			 
+		 	description : 'En la Clinica Comfenalco, ofrecemos el mejor servicio de UCI en colombia',
 		 	tel : 3017687,
 		 	uci:true,
 		 	urgencias:false,
+		 	image : 'assets/img/clinicas/comfenalco.jpg',		 			 	
 		 	doctors : [
 		 	        {_id : '98127621612891029asa7612', name:'Juan Alberto', especiality : 'Ortopeedia'},
 		 	        {_id : '98127621612891029asa7612', name:'Janna Zakzuk', especiality : 'Cardiología'},
@@ -101,7 +106,10 @@ function API(){
 		 	{
 
 
-		 			var index = Math.floor((Math.random() * 4) + 0);
+		 			var index = Math.floor((Math.random() * 5) + 0);
+
+
+
 		 			
 		 			centers[index].display = centers[index].name; //middleware fake	
 
